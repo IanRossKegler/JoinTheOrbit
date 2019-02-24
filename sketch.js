4e97f4a752c22952b5ba04cf337b78a33d59c91d
IanRossKegler <40355198+IanRossKegler@users.noreply.github.com>
2019-02-24 21:15:18 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var time = 0;
+
+function circ_x(time){
+    var x = windowWidth/2 - (windowWidth/3)*Math.cos(time);
+    return x;
+}
+
+function circ_y(time){
+    var y = windowHeight/2 + (windowHeight/6)*Math.cos(time);
+    return y;
+}
+
+function preload() {
+    font = loadFont('Cube.ttf');
+}  
+
+function setup() {    
+    createCanvas(windowWidth, windowHeight);
+    textFont(font);
+    textSize(windowWidth/26);
+    textAlign(CENTER, CENTER);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(255, 20, 140);
+    
+    if (Math.sin(time) >= 0) {
+        fill(0, 0, 0);
+        noStroke();
+        text('JOIN THE ORBIT', windowWidth/2, windowHeight/2);
+        fill(0, 225, 225);
+        circle(circ_x(time), circ_y(time), windowWidth/45);
+        time = time + 0.015;
+    }
+
+    else {      
+        noStroke(); 
+        fill(0, 225, 225);
+        circle(circ_x(time), circ_y(time), windowWidth/45);
+        fill(0, 0, 0);
+        text('JOIN THE ORBIT', windowWidth/2, windowHeight/2);
+        time = time + 0.015;
+    }
+
+    
+}
+
+function mousePressed(){
+		background(120, 20, 80);
+
+}
